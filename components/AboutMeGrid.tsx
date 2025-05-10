9aee3e7367a0aeb84f7862e13e9cdb4f45acf8e5
Ayush Dixit <fsayush100@gmail.com>
2025-05-10 22:07:28 +0530

    added code snippet

--- components/AboutMeGrid.tsx
+++ components/AboutMeGrid.tsx
@@ -1,32 +1,37 @@
 import React from "react";
 import TerminalComponent from "./TerminalComponent";
 import OrbitComponent from "./OrbitComponent";
+import { IconCloudDemo } from "./IconCloud";
+import CodeComponent from "./CodeComponent";
 
 const AboutMeGrid = () => {
   return (
     <div className="flex justify-center mb-12 items-center max-w-[1200px] mx-auto mt-12 w-full">
       <div className="grid grid-cols-3 gap-4 w-full">
         <div className="flex flex-col gap-4 w-full">
-          <div className="w-full h-[260px] rounded-3xl border border-white/20 overflow-hidden transition-transform duration-300 ease-[cubic-bezier(0.4,0,0.2,1)] hover:scale-[1.02] hover:shadow-xl will-change-transform"></div>
+          <div className="w-full h-[260px] rounded-3xl border border-white/20 overflow-hidden transition-transform duration-300 ease-[cubic-bezier(0.4,0,0.2,1)] hover:scale-[1.02] hover:shadow-xl will-change-transform">
+            <IconCloudDemo />
+          </div>
 
           <div className="w-full h-[380px] rounded-3xl border border-white/20 overflow-hidden transition-transform duration-300 ease-[cubic-bezier(0.4,0,0.2,1)] hover:scale-[1.02] hover:shadow-xl will-change-transform">
-            <OrbitComponent />
+
           </div>
         </div>
         <div className="flex flex-col gap-4 w-full">
           <div className="w-full h-[320px] rounded-3xl border border-white/20 overflow-hidden transition-transform duration-300 ease-[cubic-bezier(0.4,0,0.2,1)] hover:scale-[1.02] hover:shadow-xl will-change-transform">
-
+                    <TerminalComponent />
           </div>
 
           <div className="w-full h-[320px] rounded-3xl border border-white/20 overflow-hidden transition-transform duration-300 ease-[cubic-bezier(0.4,0,0.2,1)] hover:scale-[1.02] hover:shadow-xl will-change-transform">
-            <TerminalComponent /></div>
+</div>
         </div>
         <div className="flex flex-col gap-4 w-full">
           <div className="w-full h-[380px] rounded-3xl border border-white/20 overflow-hidden transition-transform duration-300 ease-[cubic-bezier(0.4,0,0.2,1)] hover:scale-[1.02] hover:shadow-xl will-change-transform">
+            <OrbitComponent /> </div>
 
+          <div className="w-full h-[260px] rounded-3xl border border-white/20 overflow-hidden transition-transform duration-300 ease-[cubic-bezier(0.4,0,0.2,1)] hover:scale-[1.02] hover:shadow-xl will-change-transform">
+          <CodeComponent />
           </div>
-
-          <div className="w-full h-[260px] rounded-3xl border border-white/20 overflow-hidden transition-transform duration-300 ease-[cubic-bezier(0.4,0,0.2,1)] hover:scale-[1.02] hover:shadow-xl will-change-transform"></div>
         </div>
       </div>
     </div>
---
+++ components/CodeComponent.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import Image from 'next/image';
+import React, { memo } from 'react';
+import code from "@/public/assets/codesnippet.png"
+
+{/* <iframe
+                src="https://carbon.now.sh/embed?bg=rgba%28171%2C+184%2C+195%2C+1%29&t=vscode&wt=none&l=application%2Ftypescript&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=0px&ph=0px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=export%2520const%2520asyncHandler%2520%253D%2520%28fn%29%2520%253D%253E%257B%250A%2520%2520%2520%2520return%2520async%2520%28req%252Cres%252Cnext%29%253D%253E%257B%250A%2520%2520%2520%2520%2520%2520%2520%2520try%257B%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520await%2520fn%28req%252Cres%252Cnext%29%253B%250A%2520%2520%2520%2520%2520%2520%2520%2520%257Dcatch%28error%29%257B%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520res.status%28error.code%2520%257C%257C%2520500%29.json%28%257B%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520success%253A%2520false%252C%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520message%253Aerror.message%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%257D%29%250A%250A%2520%2520%2520%2520%2520%2520%2520%2520%257D%250A%2520%2520%2520%2520%257D%250A%257D"
+                style={{
+                    width: '100%',
+                    height: '100%',
+                    border: 0,
+                    overflow: 'hidden',
+                }}
+              
+                loading="lazy"
+            /> */}
+
+const CodeComponent = () => {
+    return (
+        <div className="w-full h-full overflow-hidden rounded-3xl border border-white/20 shadow-lg">
+
+            <Image src={code} alt='code' className='w-full h-full object-cover' />
+        </div>
+    );
+};
+
+export default memo(CodeComponent);
