d84dae6ec8bf1eda4715846678a56374e853b15c
Ayush Dixit <fsayush100@gmail.com>
2025-05-13 12:55:13 +0530

    safari component added

--- components/ProjectForLanding.tsx
+++ components/ProjectForLanding.tsx
@@ -1,76 +1,5 @@
-// import Image from "next/image";
-// import React from "react";
-// import ChatImage from "../public/assets/chatproject.png";
-
-// const ProjectForLanding = () => {
-//   return (
-//     <div className="text-white/70 w-full">
-//       <div className="flex gap-10 w-full">
-//         <div className="w-[60%]">
-//           <Image
-//             src={ChatImage}
-//             alt="chatproject"
-//             className="w-full h-full object-cover"
-//           />
-//         </div>
-//         <div className="w-[40%]">
-//           <div className="w-full flex flex-col gap-4">
-//             <div className="text-xl font-semibold text-white/90">
-//               Scalable Chat Application
-//             </div>
-//             <div className="text-white/80">
-//               A fast and secure real-time chat application that enables users to
-//               send and receive instant messages, share media, and stay connected
-//               across devices with seamless synchronization and live
-//               notifications.
-//             </div>
-//             <ul className="flex flex-col px-4 gap-2">
-//               {[
-//                 "Leveraged Partial Prerendering and After for faster loading.",
-//                 "Implemented secure real-time messaging with Socket.IO.",
-//                 "Used Redis and RabbitMQ for efficient background processing.",
-//                 "Integrated NextAuth for seamless authentication.",
-//               ].map((point, idx) => (
-//                 <li key={idx} className="flex text-[15px] text-[#8C8C8C] items-start gap-2">
-//                   <span>⭐</span>
-//                   <span>{point}</span>
-//                 </li>
-//               ))}
-//             </ul>
-//             <div className="flex flex-wrap gap-2 mt-4">
-//               {[
-//                 "Next.js",
-//                 "Tailwind CSS",
-//                 "Zustand",
-//                 "React Query",
-//                 "NextAuth",
-//                 "TypeScript",
-//                 "Express.js",
-//                 "MongoDB",
-//                 "Redis",
-//                 "RabbitMQ",
-//                 "Socket.IO",
-//               ].map((tech) => (
-//                 <span
-//                   key={tech}
-//                   className="px-3 py-1 text-sm font-medium text-white bg-blue-600/80 rounded-full"
-//                 >
-//                   {tech}
-//                 </span>
-//               ))}
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ProjectForLanding;
-
-import Image from "next/image";
-import React from "react";
-import ChatImage from "../public/assets/chatproject.png";
+import React from "react"
+import { Safari } from "./magicui/safari";
 
 const techStack = [
   "Next.js",
@@ -101,13 +30,19 @@ const ProjectForLanding = () => {
           key={index}
           className="w-full text-white/70  px-4 py-12 grid md:grid-cols-2 gap-10 items-center">
 
-          <div className="relative w-full h-full rounded-xl overflow-hidden shadow-lg">
+          {/* <div className="relative w-full h-full rounded-xl overflow-hidden shadow-lg">
             <Image
               src={ChatImage}
               alt="Chat Project"
               className="rounded-xl w-full h-full object-cover transition-transform duration-300 transform hover:scale-105"
             />
+          </div> */}
+
+          <div className="relative w-full h-full">
+            <Safari url="https://chatapp.ayushdixit.site" imageSrc="https://media.istockphoto.com/id/1317323736/photo/a-view-up-into-the-trees-direction-sky.jpg?s=612x612&w=0&k=20&c=i4HYO7xhao7CkGy7Zc_8XSNX_iqG0vAwNsrH1ERmw2Q=" className="size-full" />
           </div>
+
+
 
           <article className="pl-10">
             <h2 className="text-2xl text-initial bg-gradient-white-top font-bold mb-4">
